5e6ec706c4ade5083ba02736c6ee53cb9b18532d
Sztorm <Marcinwicher99@gmail.com>
2023-04-21 23:05:12 +0200

    Updated Flags16 type with documentation.

--- src/commonMain/kotlin/com/sztorm/nonallocmath/Flags16.kt
+++ src/commonMain/kotlin/com/sztorm/nonallocmath/Flags16.kt
@@ -5,38 +5,67 @@ package com.sztorm.nonallocmath
 import kotlin.jvm.JvmInline
 import kotlin.jvm.JvmStatic
 
+/**
+ * Represents a collection of sixteen bit flags.
+ *
+ * On the JVM, non-nullable values of this type are represented as values of the primitive type
+ * `short`.
+ *
+ * @property shortValue Returns 16-bit signed integer representation of this type.
+ */
 @JvmInline
 value class Flags16(val shortValue: Short) {
+
+    /** Returns 16-bit unsigned integer representation of this type. **/
     inline val uShortValue: UShort
         get() = shortValue.toUShort()
 
+    /** Returns a value indicating whether the bit at [index] is set to 1.**/
     inline operator fun get(index: Int): Boolean = ((shortValue.toInt() ushr index) and 1) != 0
 
+    /** Returns a copy of this flags instance with the specified [flags] added. **/
     inline infix fun adding(flags: Flags16) =
         Flags16((shortValue.toInt() or flags.shortValue.toInt()).toShort())
 
+    /** Returns a copy of this flags instance with the specified [flags] removed. **/
     inline infix fun removing(flags: Flags16) =
         Flags16((shortValue.toInt() and flags.shortValue.toInt().inv()).toShort())
 
+    /** Returns a copy of this flags instance with the specified [flags] toggled. **/
     inline infix fun toggling(flags: Flags16) =
         Flags16((shortValue.toInt() xor flags.shortValue.toInt()).toShort())
 
+    /**
+     * Returns a copy of this flags instance with the specified [flags] set to the particular
+     * value.
+     */
     inline fun setting(flags: Flags16, to: Boolean) =
         Flags16(((shortValue.toInt() and flags.shortValue.toInt().inv()) or
                 (-(if (to) 1 else 0) and flags.shortValue.toInt())).toShort())
 
+    /** Returns a value indicating whether this flags instance has all the specified [flags]. **/
     inline fun hasAll(flags: Flags16) =
         (shortValue.toInt() and flags.shortValue.toInt()) == flags.shortValue.toInt()
 
+    /**
+     * Returns a value indicating whether this flags instance has any of the specified [flags].
+     */
     inline fun hasAny(flags: Flags16) = (shortValue.toInt() and flags.shortValue.toInt()) != 0
 
     companion object {
-        const val SIZE_BYTES: Int = 2
+        /** The number of bits used to represent an instance of [Flags16] in a binary form. **/
         const val SIZE_BITS: Int = 16
 
+        /** The number of bytes used to represent an instance of [Flags16] in a binary form. **/
+        const val SIZE_BYTES: Int = 2
+
+        /**
+         * Creates a new [Flags16] instance using the specified unsigned 16-bit integer [value].
+         */
         @JvmStatic
         inline fun fromUShort(value: UShort) = Flags16(value.toShort())
 
+        /** Creates a new [Flags16] instance using the specified signed 16-bit integer [value]. **/
         @JvmStatic
         inline fun fromShort(value: Short) = Flags16(value)
     }
