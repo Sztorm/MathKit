8a6542aa0bfa30aec38fb7b8697b1f1cb0ec5f1b
Sztorm <Marcinwicher99@gmail.com>
2023-04-23 01:52:06 +0200

    Added documentation for iterators.

--- src/commonMain/kotlin/com/sztorm/nonallocmath/Vector2FIterator.kt
+++ src/commonMain/kotlin/com/sztorm/nonallocmath/Vector2FIterator.kt
@@ -1,6 +1,9 @@
 package com.sztorm.nonallocmath
 
+/** An iterator over a sequence of values of type [Vector2F] **/
 abstract class Vector2FIterator : Iterator<Vector2F> {
+
+    /** Returns the next value in the sequence. **/
     final override fun next() = nextVector2F()
 
     /** Returns the next value in the sequence without boxing. **/
--- src/commonMain/kotlin/com/sztorm/nonallocmath/Vector2IIterator.kt
+++ src/commonMain/kotlin/com/sztorm/nonallocmath/Vector2IIterator.kt
@@ -1,6 +1,9 @@
 package com.sztorm.nonallocmath
 
+/** An iterator over a sequence of values of type [Vector2I] **/
 abstract class Vector2IIterator : Iterator<Vector2I> {
+
+    /** Returns the next value in the sequence. **/
     final override fun next() = nextVector2I()
 
     /** Returns the next value in the sequence without boxing. **/
