1b98568f580191661770fff13fa0437be015848d
Sztorm <Marcinwicher99@gmail.com>
2023-08-20 20:13:11 +0200

    Added Vector2IListIterator type, updated docs of iterator types.

--- src/commonMain/kotlin/com/sztorm/lowallocmath/Vector2FIterator.kt
+++ src/commonMain/kotlin/com/sztorm/lowallocmath/Vector2FIterator.kt
@@ -2,10 +2,8 @@ package com.sztorm.lowallocmath
 
 /** An iterator over a sequence of values of type [Vector2F] **/
 abstract class Vector2FIterator : Iterator<Vector2F> {
-
-    /** Returns the next value in the sequence. **/
     final override fun next() = nextVector2F()
 
-    /** Returns the next value in the sequence without boxing. **/
+    /** Returns the next element in the iteration without boxing. **/
     abstract fun nextVector2F(): Vector2F
 }
--- src/commonMain/kotlin/com/sztorm/lowallocmath/Vector2IIterator.kt
+++ src/commonMain/kotlin/com/sztorm/lowallocmath/Vector2IIterator.kt
@@ -2,10 +2,8 @@ package com.sztorm.lowallocmath
 
 /** An iterator over a sequence of values of type [Vector2I] **/
 abstract class Vector2IIterator : Iterator<Vector2I> {
-
-    /** Returns the next value in the sequence. **/
     final override fun next() = nextVector2I()
 
-    /** Returns the next value in the sequence without boxing. **/
+    /** Returns the next element in the iteration without boxing. **/
     abstract fun nextVector2I(): Vector2I
 }
