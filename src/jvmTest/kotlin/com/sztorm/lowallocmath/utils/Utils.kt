a700de8d6172915daacfcf7bdfe12030afa41b64
Sztorm <Marcinwicher99@gmail.com>
2024-01-28 23:05:10 +0100

    Updated assertApproximation test methods to match identical mathematical symbols.

--- src/jvmTest/kotlin/com/sztorm/lowallocmath/utils/Utils.kt
+++ src/jvmTest/kotlin/com/sztorm/lowallocmath/utils/Utils.kt
@@ -4,31 +4,48 @@ import com.sztorm.lowallocmath.AngleF
 import com.sztorm.lowallocmath.ComplexF
 import com.sztorm.lowallocmath.Vector2F
 import com.sztorm.lowallocmath.isApproximately
+import kotlin.math.sign
 import kotlin.test.assertTrue
 
+private fun Float.hasSymbolIdenticalTo(other: Float) =
+    (isNaN() and other.isNaN()) || (isInfinite() && other.isInfinite() && sign == other.sign)
+
 fun assertApproximation(expected: Float, actual: Float, tolerance: Float = 0.00001f) = assertTrue(
-    expected.isApproximately(actual, tolerance),
+    expected.hasSymbolIdenticalTo(actual) || expected.isApproximately(actual, tolerance),
     "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
 )
 
 fun assertApproximation(
     expected: Vector2F, actual: Vector2F, tolerance: Float = 0.00001f
-) = assertTrue(
-    expected.isApproximately(actual, tolerance),
-    "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
-)
+) {
+    val (eX: Float, eY: Float) = expected
+    val (aX: Float, aY: Float) = actual
+
+    assertTrue(
+        (eX.hasSymbolIdenticalTo(aX) || eX.isApproximately(aX, tolerance)) &&
+                (eY.hasSymbolIdenticalTo(aY) || eY.isApproximately(aY, tolerance)),
+        "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
+    )
+}
 
 fun assertApproximation(
     expected: ComplexF, actual: ComplexF, tolerance: Float = 0.00001f
-) = assertTrue(
-    expected.isApproximately(actual, tolerance),
-    "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
-)
+) {
+    val (eR: Float, eI: Float) = expected
+    val (aR: Float, aI: Float) = actual
+
+    assertTrue(
+        (eR.hasSymbolIdenticalTo(aR) || eR.isApproximately(aR, tolerance)) &&
+                (eI.hasSymbolIdenticalTo(aI) || eI.isApproximately(aI, tolerance)),
+        "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
+    )
+}
 
 fun assertApproximation(
     expected: AngleF, actual: AngleF, tolerance: AngleF = AngleF(0.00001f)
 ) = assertTrue(
-    expected.isApproximately(actual, tolerance),
+    expected.radians.hasSymbolIdenticalTo(actual.radians) ||
+            expected.isApproximately(actual, tolerance),
     "Expected <$expected> with absolute tolerance <$tolerance>, actual <$actual>."
 )
 
